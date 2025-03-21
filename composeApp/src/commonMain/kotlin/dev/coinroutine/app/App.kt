610c916f0b6ea305d472f0c843d1ddf702221df4
Amir <amirdorri81@gmail.com>
2025-03-21 12:10:49 +0330

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/dev/coinroutine/app/App.kt
@@ -0,0 +1,18 @@
+package dev.coinroutine.app
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import dev.coinroutine.app.theme.CoinRoutineTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun App() {
+
+    CoinRoutineTheme {
+
+    }
+}
